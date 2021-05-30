66d57f96667b6b2b6c8367a0d77ddf1c6e03fc40
Ehab Ebrahem <40669600+ehabebrahim@users.noreply.github.com>
2021-05-30 10:32:19 +0200

    Update test.c

--- test.c
+++ test.c
@@ -25,27 +25,3 @@ void Print_String(char * s)
 	while(*s)
 		Print_Char(*(s++));
 }
-/*
-char *Read_String(char d)
-{
-	int string_size =0;
-	char *string= (char*)calloc(10,sizeof(char));
-	char c=Read_Char();
-	Print_Char(c);
-	
-	while(c!=d)
-	{
-		*(string+string_size)= c;  //push at the end of the array
-		string_size++;
-		
-		if((string_size%10)==0)      //memory exceed
-			string =(char*)realloc(string,(string_size+10)*sizeof(char));
-		c= Read_Char();
-		Print_Char(c);
-		
-	}
-	if(string_size==0)
-		return '\0';   //null
-	return string;
-}
-*/
